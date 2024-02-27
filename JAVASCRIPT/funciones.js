ea14676a4b0c7f97e3d53c8e14b78de77daaa583
Daniel Morales Arias <dmorales.vrgroup@bicevida.cl>
2024-02-27 19:45:17 -0300

    agregando funciones

--- JAVASCRIPT/funciones.js
+++ JAVASCRIPT/funciones.js
@@ -1,20 +1,42 @@
-const sum = (p1,p2) =>{
+const sum = (p1, p2) => {
     console.log(p1)
     console.log(p2)
     return p1 + p2
 }
-const result = sum(1,4)
+const result = sum(1, 4)
 console.log(result)
 
 
-const square = p =>{
+const square = p => {
     console.log(p)
     return p * p
 }
-const result2 =square(2)
-console.log(result2)
+const result2 = square(2)
+console.log(result2);
 
 
-const t = [1,2,3]
+const t = [1, 2, 3]
 const tSquared = t.map(p => p * p)
-console.log(tSquared)
+console.log(tSquared);
+
+
+//funciones anomimas
+const suma = function (a, b) {
+    return a + b
+}
+
+const resta = function (a, b) {
+    return a - b
+}
+
+
+function operacionAsincrona(callback) {
+
+    callback();
+}
+
+function miCallback() {
+    console.log('Se ejecuto mi callback');
+}
+
+operacionAsincrona(miCallback);
